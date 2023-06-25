46adeff41f4bafab4abe28ac9f82f7d2ff5d327b
Collins Edim <collinsedim@gmail.com>
2023-06-25 15:00:45 +0100

    resolved bg-image issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,jpg,jpeg}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       backgroundImage: {
-        heroBg: "url(./src/components/assets/hero-bg.jpeg)",
+        heroBg: "url(/src/components/assets/hero-bg.jpeg)",
       },
     },
   },
