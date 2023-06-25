b4db8a537b3c58154286ab1cb1fd35529ace93cb
Collins Edim <collinsedim@gmail.com>
2023-06-26 00:24:33 +0100

    optimized hero-bg image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,10 @@ const Footer = () => {
     <div className="mt-10">
       <div className="px-4 py-2">
         <hr className="border-slate-400 mb-2" />
-        <footer className="">
+        <footer
+          className="cursor-pointer"
+          onClick={() => window.open("https://collinsedim.com", "_blank")}
+        >
           &copy; abz.agency specifically for the test task{" "}
         </footer>
       </div>
